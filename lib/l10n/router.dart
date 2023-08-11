3cd84ebefeafeaec4244a2852061cd5331285623
chanbeenkim <kcb9582@naver.com>
2023-08-11 23:09:11 +0900

    camera fail...

--- lib/l10n/router.dart
+++ lib/l10n/router.dart
@@ -1,39 +1,12 @@
 import 'package:go_router/go_router.dart';
-import 'package:tiktok_clone/features/authentication/birthday_screen.dart';
-import 'package:tiktok_clone/features/authentication/email_screen.dart';
-import 'package:tiktok_clone/features/authentication/login_screen.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
-import 'package:tiktok_clone/features/authentication/username_screen.dart';
-import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
-import 'package:tiktok_clone/features/onboarding/widgets/tutorial_screen.dart';
 
-import '../features/tab_navigation/main_navigation/main_navigation_screen.dart';
+import '../features/videos/widgets/video_recording_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-      path: SignUpScreen.routeName,
-      builder: (context, state) => const SignUpScreen(),
-    ),
-    GoRoute(
-      path: LoginScreen.routeName,
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: UsernameScreen.routeName,
-      builder: (context, state) => const UsernameScreen(),
-    ),
-    GoRoute(
-      path: EmailScreen.routeName,
-      builder: (context, state) => const EmailScreen(),
-    ),
-    GoRoute(
-      path: BirthdayScreen.routeName,
-      builder: (context, state) => const InterestsScreen(),
-    ),
-    GoRoute(
-      path: TutorialScreen.routeName,
-      builder: (context, state) => const MainNavigationScreen(),
+      path: "/",
+      builder: (context, state) => const VideoRecordingScreen(),
     )
   ],
 );
